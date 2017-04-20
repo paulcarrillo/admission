775fd33edc8fccfd7f0ba71c7337c5412790f888
Paul Carrillo <paulcarrillo05@gmail.com>
2017-04-19 18:08:09 -0600

    created basic layout using createClass method

--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,7 @@
     paths: {
       html: './src/*.html',
       js: './src/**/*.js',
+      images: './src/images/*',
       css: [
         'node_modules/bootstrap/dist/css/bootstrap.min.css',
         'node_modules/bootstrap/dist/css/bootstrap-theme.min.css'
@@ -61,6 +62,19 @@
     .pipe(gulp.dest(config.paths.dist + '/css'));
   })
 
+  // Migrates images to dist folder
+  // Could optimize images here
+  gulp.task('images', function() {
+    gulp.src(config.paths.images)
+      .pipe(gulp.dest(config.paths.dist + '/images'))
+      .pipe(connect.reload());
+
+      //publish favicon
+      gulp.src('./src/favicon/ico')
+        .pipe(gulp.dest(config.paths.dist));
+  });
+
+
   gulp.task('lint', function() {
     return gulp.src(config.paths.js)
     .pipe(lint({config: 'eslint.config.json'}))
@@ -72,4 +86,4 @@
     gulp.watch(config.paths.js, ['js', 'lint']);
   });
 
-  gulp.task('default', ['html', 'js', 'css', 'open', 'watch']);
+  gulp.task('default', ['html', 'js', 'css', 'images', 'open', 'watch']);
